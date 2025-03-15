27590e83a469b3dc85212c37145a84e64d842f79
jayamalsigera <veelochanas@gmail.com>
2025-03-15 12:19:40 +0100

    update

--- other_params.m
+++ other_params.m
@@ -1,6 +1,9 @@
-R_s = 0;
-N = 14;
-t_sf = 1.0e-2;
-B_eq = 2.0e-6;
-R_eq = mot.R + R_s;
-J_eq = mot.J + mld.J/N^2;
+R_s = 0;                    % Shunt Resistance
+N = 14;                     % Number of Poles
+t_sf = 1.0e-2;              % Static Friction Coefficient
+B_eq = 2.0e-6;              % Equivalent Friction Coefficient
+R_eq = mot.R + R_s;         % Equivalent Resistance (Shunt Resistance + Armature Resistance)
+J_eq = mot.J + mld.J/N^2;   % Equivalent Moment of Inertia
+q_en_1 = 360/(500*4);       % Quantization for all except MOTORE 8 and MOTORE 10
+q_en_2 = 360/(1024*4);      % Quantization for MOTORE 8 and MOTORE 10
+q_dac = 20 / (2^16 - 1);    % Quantization for DAC
